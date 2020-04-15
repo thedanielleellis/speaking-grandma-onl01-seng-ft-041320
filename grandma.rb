aceca26080643e15713bc3f94c0155f08a540e56
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-15 04:40:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(greeting)
   if greeting.upcase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elseif greeting.upcase == "WHAT DID YOU EAT TODAY?"
+  elsif greeting.upcase == "WHAT DID YOU EAT TODAY?"
      puts "NO, NOT SINCE 1938!"
   else
      puts "HUH?! SPEAK UP, SONNY!"
