a5135c6e556a45e628a7815bc83cc64b0c6f6bd9
Danielle Ellis <thedanielleellis@gmail.com>
2020-04-15 04:52:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(greeting)
-  if greeting.upcase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+  if greeting.upcase == 'I LOVE YOU GRANDMA!'
+    'I LOVE YOU TOO PUMPKIN!'
   elsif greeting.upcase == greeting
-     puts "NO, NOT SINCE 1938!"
+     'NO, NOT SINCE 1938!'
   else
-     puts "HUH?! SPEAK UP, SONNY!"
+     'HUH?! SPEAK UP, SONNY!'
   end 
 end
